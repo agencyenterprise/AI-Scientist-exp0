182a58de2a9e0683dccfb8f617bb309dc78e9006
Joao Meyer <1994meyer@gmail.com>
2025-11-24 17:19:23 -0300

    Removed more code

--- orchestrator/app/overview/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getOverviewData } from "@/lib/data/overview"
-import { OverviewPageClient } from "@/components/OverviewPageClient"
-
-export const dynamic = "force-dynamic"
-
-export default async function OverviewPage() {
-  const data = await getOverviewData()
-  return <OverviewPageClient initialData={data} />
-}
