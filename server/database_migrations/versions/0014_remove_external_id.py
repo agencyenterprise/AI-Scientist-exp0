0d2f266716652231eef4dedf26afd93f248ce491
Allan Girao <allan.girao@ae.studio>
2025-12-09 13:13:40 -0300

    Fix migrations order.

--- server/database_migrations/versions/0014_remove_external_id.py
+++ server/database_migrations/versions/0014_remove_external_id.py
@@ -1,7 +1,7 @@
 """Remove external id from chat_summaries and imported_conversation_summaries tables
 
-Revision ID: 0013
-Revises: 0012
+Revision ID: 0014
+Revises: 0013
 Create Date: 2025-12-01
 """
 
@@ -11,8 +11,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "0013"
-down_revision: Union[str, None] = "0012"
+revision: str = "0014"
+down_revision: Union[str, None] = "0013"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
